7af65cd7a712f3af5c2633d2fc6438c0ed576f64
unknown <camash10@outlook.com>
2025-01-06 20:32:20 -0800

    Final commit for SQL Schema file.

--- Camden_Work/crowdfunding_db_schema.sql
+++ Camden_Work/crowdfunding_db_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE category (
 -- Create table for sub-categories
 CREATE TABLE subcategory (
     subcategory_id VARCHAR(255) PRIMARY KEY,
-    subcategory TEXT NOT NULL,
+    subcategory TEXT NOT NULL
 );
 
 -- Create table for contacts
@@ -15,27 +15,35 @@ CREATE TABLE contacts (
     contact_id INT PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
 -- Create table for campaign
 CREATE TABLE campaign (
     cf_id INT PRIMARY KEY,
-    contact_id INT NOT NULL FOREIGN KEY REFERENCES contacts(contact_id),
-    company_name TEXT NOT NULL,
+    contact_id INT,
+    company_name TEXT,
     description VARCHAR(255),
-    goal INT NOT NULL,
-    pledged INT NOT NULL,
-    outcome TEXT NOT NULL,
-    backers_count INT NOT NULL,
-    country TEXT NOT NULL,
-    currency TEXT NOT NULL,
+    goal INT,
+    pledged INT,
+    outcome TEXT,
+    backers_count INT,
+    country TEXT,
+    currency TEXT,
     launched_date VARCHAR,
     end_date VARCHAR,
-    staff_pick BOOLEAN NOT NULL,
-    spotlight BOOLEAN NOT NULL,
-    category TEXT NOT NULL FOREIGN KEY REFERENCES category(category),
-    subcategory TEXT NOT NULL FOREIGN KEY REFERENCES subcategory(subcategory),
-    category_id VARCHAR(255) NOT NULL FOREIGN KEY REFERENCES category(category_id),
-    subcategory_id VARCHAR(255) NOT NULL FOREIGN KEY REFERENCES subcategory(subcategory_id)
+    staff_pick BOOLEAN,
+    spotlight BOOLEAN,
+    category TEXT,
+    subcategory TEXT,
+    category_id VARCHAR(255),
+    subcategory_id VARCHAR(255),
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
 );
+
+COPY category FROM 'Resources/category.csv' DELIMITER ',' CSV HEADER;
+COPY subcategory FROM 'Resources/subcategory.csv' DELIMITER ',' CSV HEADER;
+COPY contacts FROM 'Resources/contacts.csv' DELIMITER ',' CSV HEADER;
+COPY campaign FROM 'Resources/campaign.csv' DELIMITER ',' CSV HEADER;
